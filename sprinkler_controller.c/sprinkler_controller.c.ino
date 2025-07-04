2baa58ad984c76432bc80e326d9c595757e68ac7
Mike <mallisiscool@gmail.com>
2025-07-04 09:16:27 -0400

    Just need to actually set pins

--- sprinkler_controller.c/sprinkler_controller.c.ino
+++ sprinkler_controller.c/sprinkler_controller.c.ino
@@ -1,60 +1,100 @@
 constexpr auto BAUDE_RATE = 9600;
 
-enum struct Message {
-  None,
-  Boot,
-  Begin,
-  End,
-  Error,
-  Ok,
-
-  Count
-};
-constexpr char gMessageCodes[static_cast<int>(Message::Count)] = {'N','I','B','E','X','O'};
-constexpr bool msg_is_valid(Message m) {
-  return static_cast<int>(m) >= 0 && static_cast<int>(m) < static_cast<int>(Message::Count);
-}
-char msg_to_code(Message m) {
-  if (!msg_is_valid(m)) {
-    return 'N';
-  }
-  return gMessageCodes[static_cast<int>(m)];
-}
-bool code_is_valid(char c) {
-  for (auto& mc : gMessageCodes) {
-    if (mc == c) {
-      return true;
-    }
-  }
-  return false;
-}
-Message code_to_msg(char c) {
-  if (!code_is_valid(c)) {
-    return Message::None;
-  }
-  for (int i = 0; i < static_cast<int>(Message::Count); i++) {
-    if (gMessageCodes[i] == c) {
-      return static_cast<Message>(i);
-    }
-  }
-  return Message::None;
-}
+#define MESSAGE_INVALID -1
+#define MESSAGE_BOOT 'I'
+#define MESSAGE_BEGIN 'B'
+#define MESSAGE_END 'E'
+#define MESSAGE_OK 'O'
+#define MESSAGE_ERROR 'X'
 
 void setup() {
   Serial.begin(BAUDE_RATE);
-  send_message(Message::Boot, "Booting...");
+  Serial.print(MESSAGE_BOOT);
+  Serial.println("Booting...");
 }
+
+struct Zone {
+  Zone() = default;
+  Zone(int zone) : mZone(zone) {}
+  operator int() const {
+    return mZone;
+  }
+
+  bool valid() const {
+    return mZone > 0 && mZone < 5;
+  }
+  bool begin() const {
+    return valid();
+  }
+  bool end() const {
+    return valid();
+  }
+private:
+  int mZone = 0;
+};
 
 void loop() {
   while(Serial.available() > 0) {
-    Message msg = code_to_msg(Serial.read());
-    switch(msg) {
-      case Message::Begin:
-        auto zone = Serial.read();
-        send_message(Message::Ok, "Received begin message");
+    auto msg = Serial.readString();
+    if (msg.length() <= 0) {
+      continue;
+    }
+    // Check if newline is at the end, and remove if so
+    if (msg.endsWith("\n")) {
+      msg = msg.substring(0, msg.length() - 1);
+    }
+    switch(msg[0]) {
+      case MESSAGE_BEGIN:
+        if (msg.length() <= 1) {
+          Serial.print(MESSAGE_ERROR);
+          Serial.print("No zone to begin");
+        } else {
+          Zone zone = msg[1] - '0';
+          if (zone.valid()) {
+            if (zone.begin()) {
+              Serial.print(MESSAGE_OK);
+              Serial.print("Began zone #");
+              Serial.print(zone);
+            } else {
+              Serial.print(MESSAGE_ERROR);
+              Serial.print("Couldn't begin zone #");
+              Serial.print(zone);
+            }
+          } else {
+            Serial.print(MESSAGE_ERROR);
+            Serial.print("Invalid zone #");
+            Serial.print(zone);
+          }
+        }
+        Serial.println();
+        break;
+      case MESSAGE_END:
+        if (msg.length() <= 1) {
+          Serial.print(MESSAGE_ERROR);
+          Serial.print("No zone to end");
+        } else {
+          Zone zone = msg[1] - '0';
+          if (zone.valid()) {
+            if (zone.end()) {
+              Serial.print(MESSAGE_OK);
+              Serial.print("Ended zone #");
+              Serial.print(zone);
+            } else {
+              Serial.print(MESSAGE_ERROR);
+              Serial.print("Couldn't end zone #");
+              Serial.print(zone);
+            }
+          } else {
+            Serial.print(MESSAGE_ERROR);
+            Serial.print("Invalid zone #");
+            Serial.print(zone);
+          }
+        }
+        Serial.println();
         break;
       default:
-        send_message(Message::Error, "Unknown message received.");
+        Serial.print(MESSAGE_ERROR);
+        Serial.println("Unknown message received.");
         break;
     }
   }
